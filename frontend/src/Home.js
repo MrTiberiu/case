d2c6d20142ba2aba4f5a71222a19820dbb5aeaa7
Tiberiu Minzat <mtiberiu123@gmail.com>
2023-11-30 01:06:36 +0100

    Update Home.js

--- frontend/src/Home.js
+++ frontend/src/Home.js
@@ -17,14 +17,42 @@ function Home() {
       .then((res) => navigate("/"))
       .catch((err) => console.log(err));
   };
+  // Sortering Funksjon
+  const [sortBy, setSortBy] = useState("input");
+  let sortedItems;
+
+  if (sortBy === "input") sortedItems = data;
+
+  if (sortBy === "navn")
+    sortedItems = data.slice().sort((a, b) => a.navn.localeCompare(b.navn));
+
+  if (sortBy === "etternavn")
+    sortedItems = data
+      .slice()
+      .sort((a, b) => a.etternavn.localeCompare(b.etternavn));
+
+  if (sortBy === "email")
+    sortedItems = data.slice().sort((a, b) => a.email.localeCompare(b.email));
 
   return (
     <div className="d-flex justify-content-center align-items-center bg-info vh-100">
       <div className="bg-white rounded  p-3">
         <h2>Brukere</h2>
-        <Link to="/create" className="btn btn-success">
+        <Link to="/create" className="btn btn-success ">
           Opprette ny bruker
         </Link>
+        <br></br>
+        <span>Sorter</span>
+        <select
+          className="form-select"
+          value={sortBy}
+          onChange={(e) => setSortBy(e.target.value)}
+        >
+          <option value="input">Sorter etter rekefølge</option>
+          <option value="navn">Sorter etter Navn</option>
+          <option value="etternavn">Sorter etter Etternavn</option>
+          <option value="email">Sorter etter Email</option>
+        </select>
         <table className="table">
           <thead>
             <tr>
@@ -37,7 +65,7 @@ function Home() {
             </tr>
           </thead>
           <tbody>
-            {data.map((d, i) => (
+            {sortedItems.map((d, i) => (
               <tr>
                 <td>{d.navn}</td>
                 <td>{d.etternavn}</td>
